0e1b45907b1fbd6195b9236b7000e46d39242391
Saotalangim161099 <Saotalangim161099@gmail.com>
2022-06-14 07:35:34 -0400

    Added a printout function

---
+++ src/What.java
@@ -0,0 +1,8 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class What {
+    public static int countCow(int num) {
+        System.out.println("There are 9 cows in the night eating food with me!");
+        return 1;
+    }
+}
